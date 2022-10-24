19353c82397f1bee99dd52fc1a0acabf8604118c
Shuvo Khan <shuvoprogramer@gmail.com>
2022-10-25 00:32:46 +0600

    folder stracture organize

---
+++ src/firebase/Firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB6h-SiFkVvK56oh8gQXBUZFuZmdUpUqbk",
+    authDomain: "proforce-54fb8.firebaseapp.com",
+    projectId: "proforce-54fb8",
+    storageBucket: "proforce-54fb8.appspot.com",
+    messagingSenderId: "105699513307",
+    appId: "1:105699513307:web:3f5e1ffcdacc91bcb4cdfe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
